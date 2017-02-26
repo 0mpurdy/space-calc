e12fdf9b7c650ff8abb2be0ad6fc25ae4010805a
0mpurdy <0mpurdy@gmail.com>
2017-02-26 20:19:47 +0000

    Add initial inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  timeToReach = 'Time to reach: ';
+
+  seconds;
+  hours;
+  days;
+  years;
+  millionYears;
+
+  resultSeconds;
+  resultHours;
+  resultDays;
+  resultYears;
+
+  constructor () {
+    this.seconds = 0;
+    this.hours = 0;
+    this.days = 0;
+    this.years = 0;
+  }
+
+  populateFromSeconds(seconds) {
+    this.seconds = seconds;
+    this.hours = seconds / 3600;
+    this.days = this.hours / 24;
+    this.years = this.days / 365;
+    this.millionYears = this.years / 1000000;
+    this.calcTime();
+  }
+
+  populateResultFromHours(hours) {
+    this.resultSeconds = hours *3600;
+    this.resultDays = hours / 24;
+    this.resultYears = this.resultDays / 365;
+  }
+
+  populateFromYears(years) {
+    this.populateFromSeconds(years * 365 * 24 * 3600);
+  }
+
+  calcTime() {
+this.resultHours = Math.sqrt(this.hours);
+this.populateResultFromHours(this.resultHours);
+  }
+
 }
